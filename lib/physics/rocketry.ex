c8550226c2e171770d07b37730095d1729a13b35
dreat <lukskotarek+github@gmail.com>
2016-08-16 21:55:39 +0200

    little refactor

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,40 +1,33 @@
-defmodule Calcs do
-  def rounded_to_nearest_tenth(val) do
-    Float.ceil(val, 1)
-  end
-
-  def convert_to_km(velocity) do
-    velocity / 1000
-  end
-end
-
 defmodule Physics.Rocketry do
 
+  import Calcs
+  import Physics.Laws
+
   def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6}
+    Planets.earth
       |> escape_velocity
   end
 
   def escape_velocity(:mars) do
-    %{mass: 6.39e23, radius: 3.4e6}
+    Planets.mars
       |> escape_velocity
   end
 
   def escape_velocity(:moon) do
-    %{mass: 7.35e22, radius: 1.738e6}
+    Planets.moon
       |> escape_velocity
   end
 
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> Calcs.convert_to_km
-      |> Calcs.rounded_to_nearest_tenth
+      |> to_km
+      |> to_nearest_tenth
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
     newtons_constant = 6.67e-11
-    2 * newtons_constant * mass / radius
-      |> :math.sqrt
+    2 * newtons_gravitational_constant * mass / radius
+      |> square_root
   end
 end

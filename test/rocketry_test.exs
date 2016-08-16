85c8f5f7676a1579fd651d8abae16bbe9e12c297
dreat <lukskotarek+github@gmail.com>
2016-08-16 21:44:50 +0200

    added escape velocity of Moon and Mars alongside with tests

--- test/rocketry_test.exs
+++ test/rocketry_test.exs
@@ -5,7 +5,16 @@ defmodule RocketryTest do
     assert Physics.Rocketry.escape_velocity(:earth) == 11.2
   end
 
-  test "Planet with 8x mass and 2x radius should have twice the escape velocidy" do
+  test "Planet with 8x mass and 2x radius should have twice the escape velocity" do
     assert Physics.Rocketry.escape_velocity(%{mass: 5.972e24 * 8, radius: 6.371e6 * 2}) == 22.4
   end
+
+  test "Mars' escape velocity should equal 5.1km/s" do
+    assert Physics.Rocketry.escape_velocity(:mars) == 5.1
+  end
+
+  test "Moon's escape velocity should equal 2.4km/s" do
+    assert Physics.Rocketry.escape_velocity(:moon) == 2.4
+  end
+
 end

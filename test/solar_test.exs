2e9e0502ec1a50cefa625e14b3a9afb312a7d9b6
dreat <lukskotarek+github@gmail.com>
2016-08-23 21:57:33 +0200

    added solar flare calculations and tests

--- test/solar_test.exs
+++ test/solar_test.exs
@@ -19,4 +19,25 @@ defmodule SolarTest do
   test "We have 8 solar flares", %{data: flares} do
     assert length(flares) == 8
   end
+
+  test "Classification X flare's scale should be multiplied by 1k" do
+    assert Solar.power(%{classification: :X, scale: 99, date: Timex.to_date({1859, 8, 29})}) == 99000
+  end
+
+  test "Classification M flare's scale should be multiplied by 10" do
+    assert Solar.power(%{classification: :M, scale: 5.8, date: Timex.to_date({2015, 1, 12})}) == 58
+  end
+
+  test "Classification C flare's scale should remain unchanged" do
+    assert Solar.power(%{classification: :C, scale: 2.5, date: Timex.to_date({2015, 7, 4})}) == 2.5
+  end
+
+  test "We should have only 3 really dangerous flares", %{data: flares} do
+    assert length(Solar.no_eva(flares)) == 3
+  end
+
+  test "Deadliest should return the power of the strongest flare recorded", %{data: flares} do
+    assert Solar.deadliest(flares) == 99000
+  end
+
 end
